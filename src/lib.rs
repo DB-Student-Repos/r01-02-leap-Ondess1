41824314438a5e6bd790e9f6abba1faf63dfd26d
Ondess1 <164676566+Ondess1@users.noreply.github.com>
2024-05-05 20:23:10 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
